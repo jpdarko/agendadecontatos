1a30911a3be9d5f8644c1ad9da46ab3398879cf9
João Paulo da Silva <130411286+jpdarko@users.noreply.github.com>
2023-07-19 21:03:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('form-deposito');
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+form.addEventListener('submit', function(e) {
+    let formEValido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente: ${nomeBeneficiario.value} - conta: ${numeroContaBeneficiario.value}`;
+
+    formEValido = validaNome(nomeBeneficiario.value);
+    if (formEValido) {
+        alert(mensagemSucesso);
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+        form.submit(); // Submeter o formulário após a validação
+    } else {
+        alert("O nome não está completo");
+    }
+})
+
+    console.log(form);
+
